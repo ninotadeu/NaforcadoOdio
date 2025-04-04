98633da7864f3e765d1221aadd75d3da95441f46
ninotadeu <nino.tadeu@proton.me>
2025-04-04 20:32:26 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Painel from './painel'
+import Painel from './Painel'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
